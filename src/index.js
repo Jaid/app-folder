85ee0bc6eeb514d78227a0b193039109f827ff96
Jaid <jaid.jsx@gmail.com>
2019-06-16 02:36:36 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,52 +1,18 @@
-import fs from "fs"
 import path from "path"
 
-import filenamify from "filenamify"
-import {createLogger, format, transports} from "winston"
-import "winston-daily-rotate-file"
+import fss from "@absolunet/fss"
 import appdataPath from "appdata-path"
-import fsExtra from "fs-extra"
+import {tail} from "lodash"
 
-import consoleFormat from "./consoleFormat"
-import fileFormat from "./fileFormat"
-import stringifyConsoleFormat from "./stringifyConsoleFormat"
-import stringifyFileFormat from "./stringifyFileFormat"
+import filenamify from "./filenamify"
 
-const commonRotateFileOptions = {
-  datePattern: "YYYY-MM-DD",
-  zippedArchive: true,
-  maxSize: "20m",
-  maxFiles: "14d",
-}
-
-export default name => {
-  const normalizedName = filenamify(name, {replacement: ""})
-  const appFolder = appdataPath(normalizedName)
-  const logFolder = path.join(appFolder, "log")
-  if (!fs.existsSync(logFolder)) {
-    fsExtra.mkdirpSync(logFolder)
+export default (...name) => {
+  let folder
+  if (name.length > 1) {
+    folder = path.join(appdataPath(name[0] |> filenamify), ...tail(name).map(filenamify))
+  } else {
+    folder = appdataPath(name[0] |> filenamify)
   }
-  const logger = createLogger({
-    transports: [
-      new transports.Console({
-        level: "info",
-        format: format.combine(stringifyConsoleFormat(), format.splat(), consoleFormat()),
-      }),
-      new transports.DailyRotateFile({
-        ...commonRotateFileOptions,
-        level: "debug",
-        format: format.combine(stringifyFileFormat(), format.splat(), fileFormat({includeErrors: false})),
-        filename: path.join(logFolder, `${normalizedName}_debug_%DATE%.txt`),
-      }),
-      new transports.DailyRotateFile({
-        ...commonRotateFileOptions,
-        level: "warn",
-        format: format.combine(stringifyFileFormat(), format.splat(), fileFormat()),
-        filename: path.join(logFolder, `${normalizedName}_error_%DATE%.txt`),
-      }),
-    ],
-  })
-  logger.appFolder = appFolder
-  logger.logFolder = logFolder
-  return logger
+  fss.ensureDir(folder)
+  return folder
 }
--- test/test.js
+++ test/test.js
@@ -1,28 +1,13 @@
 import path from "path"
-import fs from "fs"
 
-import appdataPath from "appdata-path"
-import moment from "moment"
-import filenamify from "filenamify"
+import fss from "@absolunet/fss"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 const {default: appFolder} = indexModule
 
 it("should run", () => {
-  const dateString = moment().format("YYYY-MM-DD")
-  const id = `${_PKG_NAME}-test` |> filenamify
-  const logger = appFolder(id)
-  expect(logger.appFolder.endsWith(id)).toBeTruthy()
-  expect(logger.logFolder.endsWith("log")).toBeTruthy()
-  logger.error("Something went wrong: %s", new Error("123"))
-  logger.warn("abc is not %s", "cba")
-  logger.info("def")
-  const logScopes = ["debug", "error"]
-  for (const logScope of logScopes) {
-    const logFile = path.join(appdataPath(id), "log", `${id}_${logScope}_${dateString}.txt`)
-    logger.info("%s log file: %s", logScope, logFile)
-    expect(fs.existsSync(logFile)).toBeTruthy()
-    const content = fs.readFileSync(logFile, "utf8")
-    expect(content.length).toBeGreaterThan(5)
-  }
+  const id = `${_PKG_NAME}-test`
+  const folder = appFolder("Jaid", id)
+  expect(folder).toMatch(id)
+  expect(fss.pathExists(folder)).toBeTruthy()
 })
